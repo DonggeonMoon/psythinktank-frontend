540222bdf44a745309bd18fa4bcbc747058e18dd
DonggeonMoon <nullsafer@naver.com>
2026-04-11 02:05:44 +0900

    chore: 데이터 조회 스크립트 수정

--- scripts/extract-data.js
+++ scripts/extract-data.js
@@ -62,7 +62,7 @@ async function fetchData(client, country, criteria) {
 async function run() {
     const client = new Client({
         host: '127.0.0.1',
-        port: 5433,
+        port: 5432,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
