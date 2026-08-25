78d91e950e6c036c84fef5e1cfd0d387cb26e725
Donggeon Moon <nullsafer@naver.com>
2026-08-25 18:54:32 +0900

    fix: 네이버 웹마스터 도구 소유권 인증

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -4,6 +4,7 @@ import type {GatsbySSR} from "gatsby"
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({setHeadComponents}) => {
     setHeadComponents([
         <meta key="google-site-verification" name="google-site-verification"
-              content="FEZ-QQqTHTmdnS8FzNs-7TOeveE9vAmN9_fs3MgIzq4"/>
+              content="FEZ-QQqTHTmdnS8FzNs-7TOeveE9vAmN9_fs3MgIzq4"/>,
+        <meta name="naver-site-verification" content="188aec7f07541b21448d05fb0b92073950c81bdc" />
     ])
 }
